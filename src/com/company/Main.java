b3ec2f6fdc40e09ee8bf65c1e0f98fb54992c3eb
PhilipNS98 <90253330+PhilipNS98@users.noreply.github.com>
2021-11-29 09:36:38 +0100

    YEP

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	    Scanner scan = new Scanner(System.in);
+	    Scanner input = new Scanner(System.in);
         MemberHandler memberHandler = new MemberHandler();
         int answer = 1;
         while (answer != 0) {
@@ -14,9 +14,10 @@ public class Main {
                     Enter 0 to exit program
                     Enter 1 to Member Menu
                     Enter 2 to ---""");
-            answer = scan.nextInt();
+            answer = input.nextInt();
             switch (answer) {
                 case 1:
+                    memberHandler.memberHandlerMenu(input);
                     break;
                 case 2:
                     break;
